2428715c5689b85044da8f61e3c6e98a45e37c6a
Alexandre Delcour <alexandre.delcour@hotmail.com>
2020-11-02 17:27:21 +0100

    Added angular materials icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -16,10 +18,12 @@ import { LoginFormComponent } from './components/login-form/login-form.component
     HomeComponent,
     LoginFormComponent
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatIconModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
